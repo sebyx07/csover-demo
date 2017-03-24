a8f88718ed46e70b7357a53fe38b0df8184e93f2
sebi <gore.sebyx@yahoo.com>
2017-03-24 17:10:37 +0200

    add login form

---
+++ frontend/app/components/login/login-form.js
@@ -0,0 +1,10 @@
+import Ember from 'ember';
+
+export default Ember.Component.extend({
+  classNames: ['login--login-form'],
+  actions: {
+    submit(){
+
+    }
+  }
+});
---
+++ frontend/app/components/login/register-form.js
@@ -0,0 +1,11 @@
+import Ember from 'ember';
+
+export default Ember.Component.extend({
+  classNames: ['login--register-form'],
+
+  actions: {
+    submit(){
+
+    }
+  }
+});
