4273e9174dcac4d7e7777cfa5d0be54d09287389
sebi <gore.sebyx@yahoo.com>
2017-03-28 22:24:02 +0300

    add the support request

--- frontend/app/components/support-requests/list-item-customer.js
+++ frontend/app/components/support-requests/list-item-customer.js
@@ -1,5 +1,13 @@
 import Ember from 'ember';
 
 export default Ember.Component.extend({
-  classNames: ['list-group-item', 'support-requests--list-item-customer']
+  classNames: ['list-group-item', 'support-requests--list-item-customer'],
+  actions: {
+    toggleView(){
+      this.toggleProperty('showDetails');
+    },
+    delete(){
+      this.get('list').send('deleteRequest', this.get('supportRequest'))
+    }
+  }
 });
--- frontend/app/components/support-requests/list.js
+++ frontend/app/components/support-requests/list.js
@@ -2,6 +2,16 @@ import Ember from 'ember';
 import DataComponent from 'crossover/mixins/data-component';
 
 export default Ember.Component.extend(DataComponent, {
+  toastr: Ember.inject.service(),
   classNames: ['panel panel-default', 'support-requests'],
-  modelName: 'support-request'
+  modelName: 'support-request',
+
+  actions: {
+    deleteRequest(supportRequest){
+      supportRequest.destroyRecord().then(() =>{
+        this.get('toastr').success('Message has been deleted');
+        this.decrementProperty('recordCount');
+      })
+    }
+  }
 });
