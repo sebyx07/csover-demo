d44a84456552f87af65951522a48271c0ebff60c
sebi <gore.sebyx@yahoo.com>
2017-03-28 14:42:27 +0300

    add log out

---
+++ frontend/app/components/page/nav-bar.js
@@ -0,0 +1,15 @@
+import Ember from 'ember';
+
+export default Ember.Component.extend({
+  router: Ember.inject.service('-routing'),
+  toastr: Ember.inject.service(),
+
+  actions: {
+    logout(){
+      Ember.$.post('/api/v1/sessions/logout').then(() => {
+        this.get('router').transitionTo('login');
+        this.get('toastr').success('Logged Out!')
+      });
+    }
+  }
+});
--- frontend/app/routes/customers-dashboard.js
+++ frontend/app/routes/customers-dashboard.js
@@ -1,4 +1,17 @@
 import Ember from 'ember';
 
 export default Ember.Route.extend({
+  currentUser: Ember.inject.service(),
+  toastr: Ember.inject.service(),
+
+  model(){
+    return new Ember.RSVP.Promise((resolve) => {
+      this.get('currentUser').getUser().then((user) => {
+        resolve({currentUser: user})
+      }).catch(() => {
+        this.transitionTo('login');
+        this.get('toastr').error('You must be logged in as a customer');
+      });
+    });
+  }
 });
